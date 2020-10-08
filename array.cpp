844976fdf72be76408c4eaec457f52d085420432
Ketan <55191181+iamketan56@users.noreply.github.com>
2020-10-08 08:18:39 +0530

    Array Basic

---
+++ array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //initalization
+    //int a[10];
+    //float b[5]={1.2,4.5,6.6,5.5,6.4};
+    //char c[5]={'a','b','c','d','e'};
+    //access the element of array;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << a[i] << endl;
+    }
+}
